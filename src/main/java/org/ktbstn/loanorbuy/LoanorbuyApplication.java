62db92349c7601581619f471ff06ce34409baaf2
Kate Boston <ktlnbstn@gmail.com>
2017-12-05 20:33:21 -0800

    Initial project set up.

---
+++ src/main/java/org/ktbstn/loanorbuy/LoanorbuyApplication.java
@@ -0,0 +1,12 @@
+package org.ktbstn.loanorbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanorbuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanorbuyApplication.class, args);
+	}
+}
