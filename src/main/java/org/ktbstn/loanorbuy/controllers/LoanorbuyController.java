09bf4fba06f4d56cfe82ed6a9702515a47a82147
Kate Boston <ktlnbstn@gmail.com>
2017-12-11 23:25:19 -0800

    Initial skeleton controller and skeleton view.

--- src/main/java/org/ktbstn/loanorbuy/controllers/LoanorbuyController.java
+++ src/main/java/org/ktbstn/loanorbuy/controllers/LoanorbuyController.java
@@ -1,14 +1,31 @@
 package org.ktbstn.loanorbuy.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
+@RequestMapping("search")
 public class LoanorbuyController {
+
+    //request path: search/
     @RequestMapping(value="")
-    @ResponseBody
-    public String index(){
-        return "Hello Test World";
+    public String index(Model model, HttpServletRequest request){
+
+        //return homepage search bar
+        model.addAttribute("title", "Search Now");
+        return "search/index";
+    }
+
+    // request path: search/results
+    @RequestMapping(value="results")
+    public String results(Model model, HttpServletRequest request){
+
+        String book = request.getParameter("book");
+        model.addAttribute("title", "Search Results");
+        return "search/results";
     }
 }
